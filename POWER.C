28895771384f883ec6ab57f98477ceeea129f5d5
Davidzuker <35991655+Davidzuker@users.noreply.github.com>
2018-02-02 16:44:06 +0530

    to calculate the power of a given number

---
+++ POWER.C
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+    int a, b;
+    long long result = 1;
+    printf("Enter a base number: ");
+    scanf("%d", &a);
+    printf("Enter an exponent: ");
+    scanf("%d", &b);
+    while (exponent != 0)
+    {
+        result *= a;
+        --b;
+    }
+    printf("Answer = %lld", result);
+    return 0;
+}
