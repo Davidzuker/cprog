e397feb2dffe597b2804fa463eaabaf1d12e5972
Davidzuker <35991655+Davidzuker@users.noreply.github.com>
2018-02-02 16:14:27 +0530

    to print hello world N times N

---
+++ Hello.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+  int c, a;
+  printf("How many times you want to display hello world?\n");
+  scanf("%d", &a);
+  for (c=1; c<=n; c++)
+     printf("Hello world!\n");
+  return 0;
+}
