fab87e29411157d5a9c96dc10248422cb60cd789
Davidzuker <35991655+Davidzuker@users.noreply.github.com>
2018-02-02 16:19:45 +0530

    to find sum of natural numbers

---
+++ NATURAL .c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    int k, i, sum = 0;
+    printf("Enter a positive integer:\n ");
+    scanf("%d",&k);
+    for(i=1; i <= n; ++i)
+    {
+        sum += i;   // sum = sum+i;
+    }
+    printf("Sum = %d",sum);
+    return 0;
+}
